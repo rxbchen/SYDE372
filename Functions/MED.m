1ec08ef9b975db6ee54ace042e7f425df875b7e0
Brian Chen <brianchen1997@gmail.com>
2019-02-10 19:09:16 -0500

    Fixed a typo

--- Functions/MED.m
+++ Functions/MED.m
@@ -7,7 +7,7 @@ function [distances] = MED(mu1, mu2, X_pt, Y_pt)
     
     for x=1:size(distance_matrix,1)
         for y=1:size(distance_matrix,2)
-            pt = [X_pt(x,y) Y(x,y)];
+            pt = [X_pt(x,y) Y_pt(x,y)];
             distance_matrix(x,y) = (euclidian_distance(pt, mu1) - euclidian_distance(pt, mu2));
         end
     end
